4872f67e7d5d6220b23bc456c65788d3e9958fbb
aabelque <aabelque@student.42.fr>
2021-03-04 16:16:28 +0100

    resolve conflict on calloc

--- srcs/calloc.c
+++ srcs/calloc.c
@@ -6,11 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/07 23:23:50 by aabelque          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2021/02/23 13:48:34 by aabelque         ###   ########.fr       */
-=======
-/*   Updated: 2021/02/24 16:12:23 by aabelque         ###   ########.fr       */
->>>>>>> refact
+/*   Updated: 2021/03/04 16:15:56 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +26,8 @@ void	ft_bzero(void *p, size_t n)
 
 void	*calloc(size_t count, size_t size)
 {
-<<<<<<< HEAD
-	void *p;
-	size_t len;
-=======
 	void	*p;
 	size_t	len;
->>>>>>> refact
 
 	len = ft_getalign(count * size, 16);
 	if (!(p = malloc(len)))
